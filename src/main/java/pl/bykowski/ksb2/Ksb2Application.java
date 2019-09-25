25b3c25271c3a494c58a166ef7be06ef4855c598
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-09-25 20:15:15 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/ksb2/Ksb2Application.java
@@ -0,0 +1,13 @@
+package pl.bykowski.ksb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ksb2Application.class, args);
+    }
+
+}
